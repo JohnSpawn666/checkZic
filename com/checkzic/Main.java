9b13b287b9a2ec45f2dec40b97aa73ad0cfe19c6
John Spawn <jolivet.anthony23666@gmail.com>
2023-04-01 17:17:07 +0200

    Récupération des données CSV du fichier

--- com/checkzic/Main.java
+++ com/checkzic/Main.java
@@ -1,9 +1,29 @@
 package com.checkzic;
 
+import com.checkzic.file.FileReader;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
+    /**
+     * Programme principal
+     *
+     * @param args Arguments java
+     */
     public static void main(String[] args) {
-        System.out.println("test");
+
+        // Récupère les données du fichier
+        FileReader fileReader = new FileReader();
+        try {
+            List<String[]> strings = fileReader.readAllLines();
+            System.out.println(strings);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
